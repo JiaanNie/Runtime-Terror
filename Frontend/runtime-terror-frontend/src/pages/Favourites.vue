4d7eed4e020de6fe112e8108b2b1e6c6655e2664
Ali Rizvi <alihusain994@yahoo.com>
2021-02-10 16:27:44 -0600

    updated Settings,Favourite pages

--- Frontend/runtime-terror-frontend/src/pages/Favourites.vue
+++ Frontend/runtime-terror-frontend/src/pages/Favourites.vue
@@ -1,10 +1,69 @@
 <template>
- <q-page class="bg-grey-2" padding>
+  <q-page class="bg-grey-2" padding>
+    <div class="q-gutter-lg row items-start justify-evenly">
+        <q-file standout bottom-slots color="grey" style="height: 140px; max-width: 150px" bg-color="grey-4" v-model="filesImages" multiple accept=".jpg, image/*" @input="uploadImages(filesImages)" >
+            <q-icon name="add_a_photo" class="absolute-center" style="height: 140px; font-size: 2em; color: #BCAAA4"/>
+        </q-file>
+        <q-img v-for= "(item, i) in urls" :key=i :src=item.image style="height: 140px; max-width: 150px"/>
+    </div>
   </q-page>
 </template>
 
 <script>
+import axios from 'axios'
+import EventBus from '../event-bus'
+// // import JSZip from 'jszip'
+// import { saveAs } from 'file-saver'
+const AWS_URL = 'http://ec2-3-97-34-208.ca-central-1.compute.amazonaws.com:5000/'
 export default {
-  name: 'Favourites'
+  name: 'PageIndex',
+  data () {
+    return {
+      tab: 'albums',
+      filesImages: null,
+      imageSourceArray: [
+        { name: 'one', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'two', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'three', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'four', image: 'https://placeimg.com/500/300/nature' }
+      ],
+      urls: [
+        { name: 'one', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'two', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'three', image: 'https://placeimg.com/500/300/nature' },
+        { name: 'four', image: 'https://placeimg.com/500/300/nature' }
+      ]
+    }
+  },
+  created () {
+    EventBus.$on('ALBUM_SEARCH', (value) => {
+      if (value === '' || value === null) {
+        this.urls = this.imageSourceArray
+        return
+      }
+      console.log('triggered', value)
+      var a = this.imageSourceArray
+      console.log('urls', a)
+      this.urls = a.filter(function (obj) {
+        return obj.name === value
+      })
+    })
+  },
+  methods: {
+    uploadImages (filesImages) {
+      var fd = new FormData()
+      for (var i = 0; i < filesImages.length; i++) {
+        fd.append('img', filesImages[i])
+        fd.append('label', 'test')
+      }
+      axios.post(AWS_URL + 'image', fd).then(res => { console.log(res) })
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+div[class="relative-position"] {
+  height: 140px !important;
+}
+</style>
--- Frontend/runtime-terror-frontend/src/pages/Settings.vue
+++ Frontend/runtime-terror-frontend/src/pages/Settings.vue
@@ -1,8 +1,26 @@
 <template>
-  <q-page class="bg-grey-2" padding>
-  </q-page>
-</template>
+  <div class="q-pa-md" style="max-width: 98%">
+    <q-list bordered class="rounded-borders">
+      <q-expansion-item
+        expand-separator
+        label="About Album Organizer"
+      >
+      </q-expansion-item>
 
+      <q-expansion-item
+        expand-separator
+        label="Help & Support"
+      >
+      </q-expansion-item>
+
+      <q-expansion-item
+        expand-separator
+        label="Privacy Policy"
+      >
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
 <script>
 export default {
   name: 'Settings'
