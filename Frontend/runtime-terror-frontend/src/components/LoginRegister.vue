8a5389637335abedc01350bc952f0c4297f36cc3
ugwuonas <s.ugwuonah@gmail.com>
2021-03-17 21:27:04 -0600

    Adding the Login and signUp code for the frontEnd

---
+++ Frontend/runtime-terror-frontend/src/components/LoginRegister.vue
@@ -0,0 +1,67 @@
+<template>
+     <q-form
+        @submit="onSubmit"
+        class="q-gutter-md"
+     >
+        <q-input
+            filled
+            v-if="tab == 'register'"
+            v-model="formData.name"
+            label="Name"
+        />
+
+        <q-input
+            filled
+            v-model="formData.email"
+            type="email"
+            label="Email"
+            lazy-rules
+        />
+        <q-input
+            filled
+            v-model="formData.password"
+            :type="isPwd ? 'password' : 'text'"
+            label="Password">
+            <template v-slot:append>
+                <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                />
+            </template>
+        </q-input>
+
+        <div class="row">
+           <q-space/>
+            <q-btn
+            :label="tab"
+            type="submit"
+            color="primary"/>
+        </div>
+    </q-form>
+</template>
+
+<script>
+export default {
+  props: ['tab'],
+  data () {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: '',
+        isPwd: true
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.tab === 'login') {
+        console.log('login the user')
+      } else {
+        console.log('register the user')
+      }
+    }
+  }
+}
+</script>
